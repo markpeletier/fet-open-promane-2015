ae5ec60666e6c827c488eab6557353d6cdb5e45d
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-08-18 09:23:30 +0200

    replace semi-colons by colons

--- debian-env.sh
+++ debian-env.sh
@@ -1,1 +1,1 @@
-export TEXINPUTS='LaTeX-proposal/base/;LaTeX-proposal/eu/;LaTeX-proposal/etc/;/usr/share/texlive/texmf-dist/tex//;.'
+export TEXINPUTS='LaTeX-proposal/base/:LaTeX-proposal/eu/:LaTeX-proposal/etc/:/usr/share/texlive/texmf-dist/tex//:.'
