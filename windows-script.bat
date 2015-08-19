904f5a9ac89ee9f1751e78a2c5a1fca2d934c84d
Pierre de Buyl <pierre.debuyl@chem.kuleuven.be>
2015-08-19 12:29:08 +0200

    Add script for windows

---
+++ windows-script.bat
@@ -0,0 +1,5 @@
+Echo Building draft for FET Open proposal
+
+set TEXINPUTS=LaTeX-proposal/base;LaTeX-proposal/etc;LaTeX-proposal/eu;.
+pdflatex -interaction nonstopmode draft
+pdflatex -interaction nonstopmode draft
